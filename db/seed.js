becf6144957b6916a46ca0b595cfd1ecf5e98288
Scott Moore <woodwindscott@yahoo.com>
2019-10-12 13:53:09 -0400

    Created functionality to bulkCreate data from Mockaroo in our tables

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,8 @@
+const approvedDataJSON = require('./approvedDataJSON');
+const parentDataJSON = require('./parentDataJSON.js');
+const studentDataJSON = require('./studentDataJSON.js');
+const teacherDataJSON = require('./teacherDataJSON.js');
+
 module.exports = (db) => {
   // Start user data creation
   db.User.create({
@@ -14,43 +19,19 @@ module.exports = (db) => {
     password: process.env.USER_PWD,
     isAdmin: false
   });
-  // Start parent data creation
-  db.Parent.create({
-    firstName: 'Joe',
-    lastName: 'Jobs',
-    email: 'j@j.co',
-    custodyRelation: true,
-    livingWith: true,
-    employer: 'trilogy',
-    streetAddress: '513 Tournament Circle',
-    city: 'Durham',
-    zipcode: 27707,
-    phoneNumber: '(012)-345-6789'
-  });
+  db.Parent.bulkCreate(parentDataJSON);
+  db.Student.bulkCreate(studentDataJSON);
+  db.Approved.bulkCreate(approvedDataJSON);
+  db.Teacher.bulkCreate(teacherDataJSON);
+
   // Start student data creation
-  db.Student.create({
-    firstName: 'Matthew',
-    lastName: 'Michaels',
-    birthDate: '2015-04-03',
-    gender: 'male',
-    ethnicity: 'Caucasian',
-    primaryLanguage: 'English',
-    emergencyContactName: 'Barbara Matthews',
-    emergencyContactPhone: '(919) 455-0987',
-    emergencyContactRelation: 'grandmother',
-    doctorName: 'Edward Pickens',
-    doctorPhone: '(919) 655-9908',
-    dentistName: 'Rocio Quinonez',
-    dentistPhone: '(919) 674-9679',
-    allergies: true,
-    allergyDescription: 'peanuts'
-  });
+
   // Start teacher data creation
-  db.Teacher.create({
-    first_name: 'Fatima',
-    last_name: 'Carabana',
-    classroom_number: 10,
-    phone_number: '(336) 675-0786',
-    email: 'fatima@gmail.com'
-  });
+  // db.Teacher.create({
+  //   first_name: 'Fatima',
+  //   last_name: 'Carabana',
+  //   classroom_number: 10,
+  //   phone_number: '(336) 675-0786',
+  //   email: 'fatima@gmail.com'
+  // });
 };
