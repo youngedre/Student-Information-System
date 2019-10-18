1d20dea3fa7cf6d7f95740236082573603b99261
Jessie S <jessie.sholler@gmail.com>
2019-10-18 13:02:50 -0400

    duplicated data bug resolved for add parent & add student sections

--- public/assets/js/parent.js
+++ public/assets/js/parent.js
@@ -1,9 +1,11 @@
 
 /* TABS */
-$('#deeplinked-tabs').foundation();
+$('#add-child-tabs').foundation();
 
 $('#parent-tabs').foundation();
 $('#my-children-tabs').foundation();
+
+
 /* Hidden elements */
 
 $('#add-parent').show();
@@ -25,9 +27,11 @@ $('#addChild').on('click', function () {
 $('#children').on('click', function () {
   $('#myChildren').show();
   $('#my-profile').hide();
+  $('#add-parent').hide();
   $('#parent-mail').hide();
   $('#add-child').hide();
   $('#my-profile').hide();
+  console.log('hello')
 });
 
 $('#myProfile').on('click', function () {
@@ -65,3 +69,41 @@ $('.userType').on('click', function (e) {
 $('#myProfile').on('click', function () {
   $('#add-parent').show();
 });
+
+/* On click events to prevent duplicate data for add_parent data table tabs*/
+
+$('#parent-2').on('click', function (){
+  $('#parent1').removeClass('is-active'); 
+  console.log('is-active class removed successfully');
+})
+
+/* On click events to prevent duplicate data for add_student data table tabs*/
+$('#child-e').on('click', function(){
+  $('#child-general').removeClass('is-active'); 
+})
+
+$('#child-a').on('click', function(){
+  $('#child-general').removeClass('is-active'); 
+})
+
+$('#child-n').on('click', function(){
+  $('#child-general').removeClass('is-active'); 
+})
+
+/* On click events to prevent duplicate data for children data table tabs*/
+var kids = $('.my-children-e');
+// var kids = $('.my-children-e');
+var panel1 = $('.panel-1');
+var panel2 = $('.panel-2');
+var panel3 = $('.panel-3');
+var panel4= $('.panel-4');
+for (var i = 0; i < 1; i++){
+    kids.on('click', function(){
+     
+        panel1.removeClass('is-active');
+        console.log("remove a class")
+    
+})
+}
+// console.log(kids)
+console.log(panel1.length)
